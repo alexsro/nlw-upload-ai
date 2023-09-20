f5b170f312e9f0c5861b909e119adc2668324b03
alex <alex000sander@gmail.com>
2023-09-20 20:55:06 -0300

    🦄 refactor(video-input-form): Separated component

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,7 @@ import { Textarea } from './components/ui/textarea'
 import { Label } from './components/ui/label'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select'
 import { Slider } from './components/ui/slider'
+import { VideoInputForm } from './components/video-input-form'
 
 function App() {
 
@@ -40,38 +41,7 @@ function App() {
             Lembre-se: você pode utulizar a variável <code className="text-violet-400">{"{transcription}"}</code> no seu prompt para adicionar o conteúdo da transcrição do video selecionado</p>
         </div>
         <aside className="w-80 space-y-6">
-          <form className="space-y-6">
-            <label 
-              htmlFor="video" 
-              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center hover:bg-primary/5"
-            >
-              <FileVideo className="w-4 h-4"/>
-              Selecione um vídeo
-            </label>
-            <input 
-              type="file" 
-              id="video" 
-              accept="video/mp4" 
-              className="sr-only"
-            />
-
-            <Separator/>
-
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">
-                Prompt de transcrição
-              </Label>
-              <Textarea 
-                id="transcription_prompt" 
-                className="h-20 leading-relaxed resize-none"
-                placeholder="Inclua palavas-chave mencionadas no video por virgula (,)"
-              />
-            </div>
-            <Button type="submit" className="w-full">
-              Carregar vídeo 
-              <Upload className="h-4 w-4 ml-2"/>
-            </Button>
-          </form>
+          <VideoInputForm />
           <Separator/>
           <form className="space-y-6">
             <div className="space-y-2">
